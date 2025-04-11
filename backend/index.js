081096f390ea958ac225c7a8f414d3098ff36386
Ankit <khairwalankit7@gmail.com>
2025-04-11 23:21:22 +0530

    chatApp

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ import cors from "cors";
 
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
+import { connectDB } from "./src/lib/db.js";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
 
@@ -18,12 +18,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
