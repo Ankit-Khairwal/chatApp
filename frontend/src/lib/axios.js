2e7b01cfca9c326e8207da4836f9546728c34b60
Ankit <khairwalankit7@gmail.com>
2025-04-12 11:12:34 +0530

    chatApp

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "https://chatapp-xq9i.onrender.com/api"
+      : "/api",
   withCredentials: true,
 });
